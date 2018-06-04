cf104fe2b7b9e5a7b6334533972e00ec6bf816b6
mbafdili <bafdili.mehdi@outlook.com>
2018-06-04 09:50:18 +0200

    debut

---
+++ resources/views/admin/post/postindex.blade.php
@@ -0,0 +1,11 @@
+@extends('adminlte::page')
+
+@section('title', 'AdminLTE')
+
+@section('content_header')
+    <h1>Tableau de Post</h1>
+@stop
+
+@section('content')
+    <p>Rédiger tout vos postes ici</p>
+@stop
